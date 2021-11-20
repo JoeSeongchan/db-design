1323c26d64a1cdc3a909653045923164003a82a2
seongchan joe <hey.jonathan.cho@gmail.com>
2021-11-20 21:44:31 +0900

    sql complete.

---
+++ dummy/old_price dummy.sql
@@ -0,0 +1,139 @@
+-- #1
+INSERT INTO
+    old_price (
+        model_name,
+        measurement_date,
+        price
+    )
+VALUES
+    (
+        'NT350XCR-AD5W1',
+        '2021-11-14',
+        900000
+    );
+
+-- #2
+INSERT INTO
+    old_price (
+        model_name,
+        measurement_date,
+        price
+    )
+VALUES
+    (
+        'NT350XCR-AD5W2',
+        '2021-11-14',
+        1000000
+    );
+
+-- #3
+INSERT INTO
+    old_price (
+        model_name,
+        measurement_date,
+        price
+    )
+VALUES
+    (
+        'NT350XCR-AD5W3',
+        '2021-11-14',
+        1400000
+    );
+
+-- #4
+INSERT INTO
+    old_price (
+        model_name,
+        measurement_date,
+        price
+    )
+VALUES
+    (
+        'NT350XCR-AD5W4',
+        '2021-11-14',
+        1200000
+    );
+
+-- #5
+INSERT INTO
+    old_price (
+        model_name,
+        measurement_date,
+        price
+    )
+VALUES
+    (
+        'NT350XCR-AD5W5',
+        '2021-11-14',
+        1900000
+    );
+
+-- #6
+INSERT INTO
+    old_price (
+        model_name,
+        measurement_date,
+        price
+    )
+VALUES
+    (
+        'NT350XCR-AD5W6',
+        '2021-11-14',
+        1800000
+    );
+
+-- #7
+INSERT INTO
+    old_price (
+        model_name,
+        measurement_date,
+        price
+    )
+VALUES
+    (
+        'MGN63KH/1',
+        '2021-11-14',
+        1150000
+    );
+
+-- #8
+INSERT INTO
+    old_price (
+        model_name,
+        measurement_date,
+        price
+    )
+VALUES
+    (
+        'MGN63KH/2',
+        '2021-11-14',
+        1200000
+    );
+
+-- #9
+INSERT INTO
+    old_price (
+        model_name,
+        measurement_date,
+        price
+    )
+VALUES
+    (
+        'MGN63KH/3',
+        '2021-11-14',
+        1450000
+    );
+
+-- #10
+INSERT INTO
+    old_price (
+        model_name,
+        measurement_date,
+        price
+    )
+VALUES
+    (
+        'MGN63KH/4',
+        '2021-11-14',
+        1150000
+    );
